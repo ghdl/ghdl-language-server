1550bf86daba56d27bd4db05ef166f4d834d45a1
Tristan Gingold <tgingold@free.fr>
2019-06-05 20:33:33 +0200

    testsuite: improve harness.

--- tests/testenv.py
+++ tests/testenv.py
@@ -12,14 +12,41 @@ class StrConn:
     def write(self, s):
         self.res += s
 
+
+def show_diffs(name, ref, res):
+    if isinstance(ref, dict) and isinstance(res, dict):
+        for k in ref:
+            if k not in res:
+                print('{}.{} not in the result'.format(name, k))
+            else:
+                show_diffs('{}.{}'.format(name, k), ref[k], res[k])
+        for k in res:
+            if k not in ref:
+                print('{}.{} unexpected in the result'.format(name, k))
+    elif isinstance(ref, str) and isinstance(res, str):
+        if res != ref:
+            print('{}: mismatch (ref: {}, result: {})'.format(name, res, ref))
+    elif isinstance(ref, int) and isinstance(res, int):
+        if res != ref:
+            print('{}: mismatch (ref: {}, result: {})'.format(name, res, ref))
+    elif isinstance(ref, list) and isinstance(res, list):
+        for i in range(min(len(ref), len(res))):
+            show_diffs('{}[{}]'.format(name, i), ref[i], res[i])
+        if len(ref) > len(res):
+            print('{}: missing elements'.format(name))
+        elif len(ref) < len(res):
+            print('{}: extra elements'.format(name))
+    else:
+        print('unhandle type {} in {}'.format(type(ref), name))
+
 def run_compare(req_name, rep_name):
-    # Convert the input file to LSP
+    # Convert the JSON input file to an LSP string.
     res = json.load(open(req_name, 'r'))
     conn = StrConn()
     ls = lsp.LanguageProtocolServer(None, conn)
     for req in res:
         ls.write_output(req)
-    
+
     # Run
     p = subprocess.run([os.environ.get('GHDLLS', '../../ghdl-ls')],
         input=conn.res.encode('utf-8'), stdout=subprocess.PIPE)
@@ -35,20 +62,32 @@ def run_compare(req_name, rep_name):
     conn = lsp.LSPConn(in_io, None)
     ls = lsp.LanguageProtocolServer(None, conn)
     ref = json.load(open(rep_name, 'r'))
+    errs = 0
+    json_res = []
     for r in ref:
         rep = ls.read_request()
         if rep is None:
             print('FAIL: number of reply does not match')
-            sys.exit(1)
+            errs += 1
+            break
         rep = json.loads(rep)
+        json_res.append(rep)
         if rep != r:
-            print('FAIL: reply does not match')
+            print('FAIL: reply does not match for {}'.format(req_name))
             print(rep)
             print(r)
-            sys.exit(1)
+            show_diffs('', r, rep)
+            errs += 1
     rep = ls.read_request()
     if rep is not None:
         print('FAIL: too many replies')
+        errs += 1
+    if errs != 0:
+        print('FAILURE between output and {} (for {})'.format(rep_name, req_name))
+        print('Writing result output to result.json')
+        with open('result.json', 'w') as f:
+            f.write(json.dumps(json_res, indent=2))
+            f.write('\n')
         sys.exit(1)
 
 def done():
